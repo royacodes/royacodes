942ff0419d1d099604144fd16c6a4468c67f80c0
royacodes <roya.raastgar@gmail.com>
2024-11-25 09:32:23 +0330

    some changes

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,47 +1,86 @@
-import React from 'react'
-import './contact.css'
-import {MdOutlineMailOutline} from 'react-icons/md'
-import {FaTelegram} from 'react-icons/fa'
-import {BsWhatsapp} from 'react-icons/bs'
+import React, { useRef } from "react";
+import "./contact.css";
+import { MdOutlineMailOutline } from "react-icons/md";
+import { FaTelegram } from "react-icons/fa";
+import { BsWhatsapp } from "react-icons/bs";
+import emailjs from "emailjs-com";
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault(); // Prevent page reload on form submission
+
+    emailjs
+      .sendForm(
+        "service_ghnd98l", // Replace with your EmailJS service ID
+        "template_f476ktu", // Replace with your EmailJS template ID
+        form.current,
+        "usUuFH-D7UQXl5D4b" // Replace with your EmailJS public key
+      )
+      .then(
+        (result) => {
+          console.log("Email sent:", result.text);
+          alert("Message sent successfully!");
+          e.target.reset(); // Reset form fields
+        },
+        (error) => {
+          console.error("Error sending email:", error.text);
+          alert("Failed to send the message. Please try again.");
+        }
+      );
+  };
+
   return (
-    <section id='contact'>
+    <section id="contact">
       <h5>Get In Touch</h5>
       <h2>Contact Me</h2>
       <div className="container conatct__container">
         <div className="contact__options">
           <article className="contact__option">
-            <MdOutlineMailOutline className='contact__option-icon'/>
+            <MdOutlineMailOutline className="contact__option-icon" />
             <h4>Email</h4>
             <h5>roya.raastgar@gmail.com</h5>
-            <a href="mailto:roya.raastgar@gmail.com"target='_blank'>Send a Message</a>
+            <a href="mailto:roya.raastgar@gmail.com" target="_blank">
+              Send a Message
+            </a>
           </article>
 
           <article className="contact__option">
-            <FaTelegram className='contact__option-icon'/>
+            <FaTelegram className="contact__option-icon" />
             <h4>Telegram</h4>
             <h5>royacodes</h5>
-            <a href="https://web.telegram.org/z/#-1441719524"target='_blank'>Send a Message</a>
+            <a href="https://web.telegram.org/z/#-1441719524" target="_blank">
+              Send a Message
+            </a>
           </article>
 
           <article className="contact__option">
-            <BsWhatsapp className='contact__option-icon'/>
+            <BsWhatsapp className="contact__option-icon" />
             <h4>Whatsapp</h4>
             <h5>+989396770887</h5>
-            <a href="https://api.whatsapp.com/send?phone" target='_blank'>Send a Message</a>
+            <a href="https://api.whatsapp.com/send?phone" target="_blank">
+              Send a Message
+            </a>
           </article>
         </div>
         {/* END OF CONTACT OPTIONS */}
-        <form action="">
-          <input type="text" name='name' placeholder='Your Ful Name' required />
-          <input type="email" name='email' placeholder='Your Email' required />
-          <textarea name="message" rows="7" placeholder='Your Message' required></textarea>
-          <button type='submit' className='btn btn-primary'>Send Message</button>
+        <form ref={form} onSubmit={sendEmail}>
+          <input type="text" name="name" placeholder="Your Ful Name" required />
+          <input type="email" name="email" placeholder="Your Email" required />
+          <textarea
+            name="message"
+            rows="7"
+            placeholder="Your Message"
+            required
+          ></textarea>
+          <button type="submit" className="btn btn-primary">
+            Send Message
+          </button>
         </form>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import './experience.css'
-import {BsPatchCheckFill} from 'react-icons/bs'
+import React from "react";
+import "./experience.css";
+import { BsPatchCheckFill } from "react-icons/bs";
 
 const Experience = () => {
   return (
-    <section id='experience'>
+    <section id="experience">
       <h5>What Skills I Have</h5>
       <h2>My Experience</h2>
       <div className="container experience__container">
@@ -12,35 +12,32 @@ const Experience = () => {
           <h3>Programming Languages</h3>
           <div className="experience__content">
             <article className="experience__details">
-              <BsPatchCheckFill className='experience__details-icon'/>
+              <BsPatchCheckFill className="experience__details-icon" />
               <div>
-              <h4>Java</h4>
-              <small className='text-light'>Experienced</small>
+                <h4>Java</h4>
+                <small className="text-light">Experienced</small>
               </div>
             </article>
             <article className="experience__details">
-              <BsPatchCheckFill className='experience__details-icon'/>
+              <BsPatchCheckFill className="experience__details-icon" />
               <div>
-              <h4>Kotlin</h4>
-              <small className='text-light'>Experienced</small>
+                <h4>Kotlin</h4>
+                <small className="text-light">Experienced</small>
               </div>
-              
             </article>
             <article className="experience__details">
-              <BsPatchCheckFill className='experience__details-icon'/>
+              <BsPatchCheckFill className="experience__details-icon" />
               <div>
-              <h4>Flutter/Dart</h4>
-              <small className='text-light'>Experienced</small>
+                <h4>Flutter/Dart</h4>
+                <small className="text-light">Experienced</small>
               </div>
-              
-            </article> 
+            </article>
             <article className="experience__details">
-              <BsPatchCheckFill className='experience__details-icon'/>
+              <BsPatchCheckFill className="experience__details-icon" />
               <div>
-              <h4>JavaScript</h4>
-              <small className='text-light'>Experienced</small>
+                <h4>React JS</h4>
+                <small className="text-light">Experienced</small>
               </div>
-              
             </article>
             {/* <article className="experience__details">
               <BsPatchCheckFill className='experience__details-icon'/>
@@ -61,38 +58,36 @@ const Experience = () => {
           </div>
         </div>
         {/* END Of Frontend */}
-              <div className="experience__backend">
-        <h3>Tools & Architectures</h3>
+        <div className="experience__backend">
+          <h3>Tools & Architectures</h3>
           <div className="experience__content">
             <article className="experience__details">
-              <BsPatchCheckFill className='experience__details-icon'/>
+              <BsPatchCheckFill className="experience__details-icon" />
               <div>
-              <h4>TDD Architecture</h4>
-              <small className='text-light'>Experienced</small>
+                <h4>TDD Architecture</h4>
+                <small className="text-light">Experienced</small>
               </div>
             </article>
             <article className="experience__details">
-              <BsPatchCheckFill className='experience__details-icon'/>
+              <BsPatchCheckFill className="experience__details-icon" />
               <div>
-              <h4>MVVM Architecture</h4>
-              <small className='text-light'>Experienced</small>
-              </div>              
+                <h4>MVVM Architecture</h4>
+                <small className="text-light">Experienced</small>
+              </div>
             </article>
             <article className="experience__details">
-              <BsPatchCheckFill className='experience__details-icon'/>
+              <BsPatchCheckFill className="experience__details-icon" />
               <div>
-              <h4>Jetpack Compose</h4>
-              <small className='text-light'>Intermediate</small>
-              </div>     
-             
-            </article> 
+                <h4>Jetpack Compose</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
+            </article>
             <article className="experience__details">
-              <BsPatchCheckFill className='experience__details-icon'/>
+              <BsPatchCheckFill className="experience__details-icon" />
               <div>
-              <h4>UI/UX principles</h4>
-              <small className='text-light'>Intermediate</small>
-              </div>    
-              
+                <h4>UI/UX principles</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
             </article>
             {/* <article className="experience__details">
               <BsPatchCheckFill className='experience__details-icon'/>
@@ -106,7 +101,7 @@ const Experience = () => {
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Experience
+export default Experience;
